cf829d3369236e004a6ffa5dd625c161519a604e
tdj-tech <330119928@qq.com>
2022-03-20 23:30:22 +0800

    测试

---
+++ tankDemo/src/com/mashibing/de/Main.java
@@ -0,0 +1,13 @@
+package com.mashibing.de;
+
+/**
+ * @author tdj
+ * 2022/3/20 0020
+ * @desc
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello , world!");
+    }
+}
